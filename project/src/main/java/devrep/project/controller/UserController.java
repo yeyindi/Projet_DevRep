68e9193be4ae694ce0c1a57c57c0f3b3b6e842cb
yeyindi <40499307+yeyindi@users.noreply.github.com>
2020-02-09 23:34:17 +0100

    Update UserController.java

--- project/src/main/java/devrep/project/controller/UserController.java
+++ project/src/main/java/devrep/project/controller/UserController.java
@@ -3,6 +3,7 @@ package devrep.project.controller;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,15 +12,19 @@ import org.springframework.web.bind.annotation.RestController;
 
 import devrep.project.interfaces.UserRepository;
 import devrep.project.model.User;
+import devrep.project.util.SendMailUtils;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:4200")
 public class UserController {
 
     private final UserRepository userRepository;
+    private final SendMailUtils MailSender;
     
     public UserController(UserRepository userRepository) {
     	this.userRepository = userRepository;
+    	MailSender = new SendMailUtils();
+    	MailSender.setInitData("admin@gmail.com", "123456789");
     }
  
     @GetMapping("/api/users")
@@ -29,6 +34,8 @@ public class UserController {
  
     @PostMapping("/api/users")
     public void addUser(@RequestBody User user) {
+    	String text = user.getfName()+""+user.getlName()+" your demanded are sended to admin, please wait for validation"; 
+    	MailSender.simpleMailSend("yindiye@gmail.com", "test", text);
         userRepository.save(user);
     }
     
@@ -37,4 +44,9 @@ public class UserController {
     	user.setConf(conf_id);
     	userRepository.save(user);
     }
+    
+    @DeleteMapping("/api/users/{id}")
+	  void deleteUser(@PathVariable Long id) {
+    	userRepository.deleteById(id);
+	}
 }
