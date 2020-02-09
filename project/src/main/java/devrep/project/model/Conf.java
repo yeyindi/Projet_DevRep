ac3c6bcc70b1a976dd934d16590879b1a8d53acb
yeyindi <40499307+yeyindi@users.noreply.github.com>
2020-02-09 22:57:29 +0100

    Update Conf.java

--- project/src/main/java/devrep/project/model/Conf.java
+++ project/src/main/java/devrep/project/model/Conf.java
@@ -8,43 +8,68 @@ import javax.persistence.Id;
 @Entity
 public class Conf {
 
+
 	@Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private String conf_id;
+    private long conf_id;
 	private final String title;
-	private final String early;
-	private final String late;
+	private final String early_price;
+	private final String early_date;
+	private final String late_price;
+	private final String late_date;
+	private final String registration_type;
 	
-	public Conf(String title, String early, String late) {
+	public Conf(String title, String early_price, String early_date, String late_price, String late_date,
+			String registration_type) {
+		super();
 		this.title = title;
-		this.early = early;
-		this.late = late;
+		this.early_price = early_price;
+		this.early_date = early_date;
+		this.late_price = late_price;
+		this.late_date = late_date;
+		this.registration_type = registration_type;
 	}
 	
 	public Conf() {
 		this.title = "";
-		this.early = "";
-		this.late = "";
+		this.early_price = "";
+		this.early_date = "";
+		this.late_price = "";
+		this.late_date = "";
+		this.registration_type = "";
 	}
 
 	public String getTitle() {
 		return title;
 	}
 
-	public String getEarly() {
-		return early;
-	}
 
-	public String getLate() {
-		return late;
-	}
-
-	public String getId() {
+	public long getId() {
 		return conf_id;
 	}
 
-	public void setId(String id) {
+	public void setId(long id) {
 		this.conf_id = id;
+	}
+
+	public String getEarly_price() {
+		return early_price;
+	}
+
+	public String getEarly_date() {
+		return early_date;
+	}
+
+	public String getLate_price() {
+		return late_price;
+	}
+
+	public String getLate_date() {
+		return late_date;
+	}
+
+	public String getRegistration_type() {
+		return registration_type;
 	}
 
 	
