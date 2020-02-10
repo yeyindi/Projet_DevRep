48a49696b4f117c83c47e602f8639e67ec1c0a7f
yeyindi <40499307+yeyindi@users.noreply.github.com>
2020-02-10 23:58:24 +0100

    Add files via upload

--- project/src/main/java/devrep/project/model/Conf.java
+++ project/src/main/java/devrep/project/model/Conf.java
@@ -13,35 +13,29 @@ public class Conf {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long conf_id;
 	private final String title;
-	private final String[] early_prices;
-	private final String early_dateFrom;
-	private final String early_dateTo;
-	private final String[] late_prices;
-	private final String late_dateFrom;
-	private final String late_dateTo;
+	private final String early_price;
+	private final String early_date;
+	private final String late_price;
+	private final String late_date;
 	private final String registration_type;
 	
-	public Conf(String title, String[] early_prices, String early_dateFrom,String early_dateTo, String[] late_prices, String late_dateFrom,
-			String late_dateTo,String registration_type) {
+	public Conf(String title, String early_price, String early_date, String late_price, String late_date,
+			String registration_type) {
 		super();
 		this.title = title;
-		this.early_prices = early_prices;
-		this.early_dateFrom = early_dateFrom;
-		this.early_dateTo = early_dateTo;
-		this.late_prices = late_prices;
-		this.late_dateFrom = late_dateFrom;
-		this.late_dateTo = late_dateTo;
+		this.early_price = early_price;
+		this.early_date = early_date;
+		this.late_price = late_price;
+		this.late_date = late_date;
 		this.registration_type = registration_type;
 	}
 	
 	public Conf() {
 		this.title = "";
-		this.early_prices = new String[5];
-		this.early_dateFrom = "";
-		this.late_prices = new String[5];
-		this.late_dateFrom = "";
-		this.early_dateTo = "";
-		this.late_dateTo = "";
+		this.early_price = "";
+		this.early_date = "";
+		this.late_price = "";
+		this.late_date = "";
 		this.registration_type = "";
 	}
 
@@ -58,26 +52,20 @@ public class Conf {
 		this.conf_id = id;
 	}
 
-	public String[] getEarly_price() {
-		return early_prices;
+	public String getEarly_price() {
+		return early_price;
 	}
 
-	public String getEarly_dateFrom() {
-		return early_dateFrom;
+	public String getEarly_date() {
+		return early_date;
 	}
 
-	public String[] getLate_price() {
-		return late_prices;
+	public String getLate_price() {
+		return late_price;
 	}
 
-	public String getLate_dateFrom() {
-		return late_dateFrom;
-	}
-	public String getLate_dateTo() {
-		return late_dateTo;
-	} 
-	public String getEarly_dateTo() {
-		return this.early_dateTo;
+	public String getLate_date() {
+		return late_date;
 	}
 
 	public String getRegistration_type() {
