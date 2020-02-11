32adcc271728524dc7c8048d65299b71275d29f2
yeyindi <40499307+yeyindi@users.noreply.github.com>
2020-02-11 16:06:45 +0100

    adding log

---
+++ project/src/main/java/devrep/project/util/LoggerUtils.java
@@ -0,0 +1,22 @@
+package devrep.project.util;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LoggerUtils {
+	
+	private final Logger logger = LoggerFactory.getLogger("rollingFileLogger");
+	
+	public LoggerUtils() {
+		logger.info("test");
+	}
+	
+	public void LogSuccess(String message) {
+		logger.info(message);
+	}
+	
+	public void LogFail(String message) {
+		logger.warn(message);
+	}
+
+}
